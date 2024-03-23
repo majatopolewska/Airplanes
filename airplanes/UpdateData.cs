b656e6132c9355e162780258f78268d7b9ba57b5
polatoja <127290541+polatoja@users.noreply.github.com>
2024-03-23 20:27:20 +0100

    ??

--- airplanes/UpdateData.cs
+++ airplanes/UpdateData.cs
@@ -29,7 +29,7 @@ namespace airplanes
         public static void UpdateFlights()
         {
             FlightsGUIData flightsGUIData = ConvertToFlightsGUIData(data);
-            Console.WriteLine($"count: {flightsGUIData.GetFlightsCount()}");
+            // Console.WriteLine($"count: {flightsGUIData.GetFlightsCount()}");
 
             FlightTrackerGUI.Runner.UpdateGUI(flightsGUIData);
         }
@@ -54,10 +54,6 @@ namespace airplanes
             (double x, double y) distanceOfFlight = Airport.CalculateDistance(origin, target);
             (double origin_x, double origin_y) = (origin.Longitude, origin.Latitude);
 
-            // PROCENT DROGI OD START DO END
-
-            
-
             DateTime takeoff = DateTime.Parse(flight.TakeoffTime);
             DateTime landing = DateTime.Parse(flight.LandingTime);
 
@@ -71,15 +67,10 @@ namespace airplanes
             double travelledDistanceX = distanceOfFlight.x * t;
             double travelledDistanceY = distanceOfFlight.y * t;
 
-            // tu tez moze byc zle przy samolotach landing<takeoff
-            // mozna sprawdzic wypisujac wszystkie i liczac reczenie z ftr
-
             WorldPosition currPosition = new WorldPosition();
             
-            currPosition.Longitude = origin_x + travelledDistanceX; //ni chuja tak nie mozna 
-            currPosition.Latitude = origin_y + travelledDistanceY; // tak samo
-            // odleglosc w (x,y) != odleglosc w (lon,lat)
-            // ponizej wypisywanie samolotow dla ktorych sa zle wspolrzedne
+            currPosition.Longitude = origin_x + travelledDistanceX;
+            currPosition.Latitude = origin_y + travelledDistanceY;
             
             if((currPosition.Longitude < -180 || currPosition.Longitude > 180) || (currPosition.Latitude > 90 || currPosition.Latitude < -90))
             {
@@ -91,7 +82,6 @@ namespace airplanes
                 }
                 if (departureTime <= DateTime.Now && landingTime >= DateTime.Now)
                     Console.WriteLine($"ID {flight.Id} Duration {flightDuration} Lon {currPosition.Longitude} Lat {currPosition.Latitude} {timeFromStart}");
-
             }
 
             return currPosition;
@@ -110,12 +100,12 @@ namespace airplanes
 
                     DateTime departureTime = DateTime.Parse(flight.TakeoffTime);
                     DateTime landingTime = DateTime.Parse(flight.LandingTime);
+
                     if(landingTime < departureTime)
                     {
                         departureTime.AddDays(-1);
                     }
-                    if (departureTime <= DateTime.Now && landingTime >= DateTime.Now) // ten if nie da ci wysztkich samolotow
-                        // bo co jak departure 23:55 i teraz jest 00:01
+                    if (departureTime <= DateTime.Now && landingTime >= DateTime.Now)
                     {
                         double angleRadians = Airport.CalculateAngle(originAirport, targetAirport);
 
